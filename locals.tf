c8056b1663eb25ed3f76ad35aebda21e83863d37
Admir Trakic <adtr@dr.dk>
2022-02-09 09:03:51 +0100

    Update

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 
 locals {
-  project                = data.terraform_remote_state.cluster.outputs.google["project_id"]
-  region                 = data.terraform_remote_state.cluster.outputs.google["region"]
-  host                   = data.terraform_remote_state.cluster.outputs.kubernetes["host"]
-  cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.kubernetes["cluster_ca_certificate"]
+  project                = data.terraform_remote_state.cluster.outputs.project_id
+  region                 = data.terraform_remote_state.cluster.outputs.region
+  host                   = data.terraform_remote_state.cluster.outputs.host
+  cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
   token                  = data.google_client_config.default.access_token
 }
 
